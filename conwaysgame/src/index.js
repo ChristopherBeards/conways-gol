4f1164adbafca1f35b5181c40e803b7d2356d3e6
Christopher Beards <beardscm@gmail.com>
2018-05-05 12:02:05 -0400

    Grid Displays

--- conwaysgame/src/index.js
+++ conwaysgame/src/index.js
@@ -2,11 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Grid extends React.Component {
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  }
+
   render() {
     return (
-      <div>
-        Grid
+      <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox} />
+    );
+  }
+}
+
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 16;
+    let rowsArr = [];
+    let boxClass = "";
+
+    //  Refactor to use map
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        // Check if true or false
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+
+    return (
+      <div className="grid" style={{width: width}}>
+        {rowsArr}
       </div>
     )
   }
@@ -15,8 +51,14 @@ class Grid extends React.Component {
 class Main extends React.Component {
   constructor() {
     super();
+
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
     }
   }
   render() {
@@ -24,6 +66,10 @@ class Main extends React.Component {
       <div>
         <h1>The Game of Life</h1>
         <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
         />
         <h2>Generations: {this.state.generation}</h2>
       </div>

650e8a16ef0da157abe1f2d130c4d69eb607bcd4
Christopher Beards <beardscm@gmail.com>
2018-05-05 12:23:40 -0400

    Grid properly seeds

--- conwaysgame/src/index.js
+++ conwaysgame/src/index.js
@@ -2,15 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Box extends React.Component {
-  selectBox = () => {
-    this.props.selectBox(this.props.row, this.props.col);
+class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
+    }
+  }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy
+    });
+  }
+
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        // randomly choose whether on or off
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy
+    })
+  }
+
+  componentDidMount() {
+    this.seed();
   }
 
   render() {
     return (
-      <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox} />
-    );
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
   }
 }
 
@@ -48,33 +93,20 @@ class Grid extends React.Component {
   }
 }
 
-class Main extends React.Component {
-  constructor() {
-    super();
-
-    this.speed = 100;
-    this.rows = 30;
-    this.cols = 50;
-
-    this.state = {
-      generation: 0,
-      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
-    }
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
   }
+
   render() {
     return (
-      <div>
-        <h1>The Game of Life</h1>
-        <Grid
-          gridFull={this.state.gridFull}
-          rows={this.rows}
-          cols={this.cols}
-          selectBox={this.selectBox}
-        />
-        <h2>Generations: {this.state.generation}</h2>
-      </div>
-    )
+      <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox} />
+    );
   }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));

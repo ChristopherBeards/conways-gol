fe7408829dc0e48a3b6576edb7ab0b958ad7ef54
Christopher Beards <beardscm@gmail.com>
2018-05-05 12:41:10 -0400

    Rules Implemented; Game Runs

--- conwaysgame/src/index.js
+++ conwaysgame/src/index.js
@@ -39,8 +39,41 @@ class Main extends React.Component {
     })
   }
 
+  playButton = () => {
+    clearInterval(this.intervalId)
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  play = () => {
+    let g = this.state.gridFull; // First copy
+    // Check what grid is currently and then change on the clone
+    let g2 = arrayClone(this.state.gridFull);
+
+    // The CGOL Rules
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0; // How many neighbors
+        if (i > 0) if (g[i - 1][j]) count ++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1,
+    })
+  }
+
   componentDidMount() {
     this.seed();
+    this.playButton();
   }
 
   render() {
